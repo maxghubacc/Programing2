f50d90c13df12cdc5e7b2690a6b83ebc458531ff
maxghubacc <maximiliangardellini@gmail.com>
2026-02-02 01:40:19 +0100

    homework 3 commit

--- Homework/Homework two.py
+++ Homework/Homework two.py
@@ -1,6 +1,6 @@
 try:
-    gross = int(input())
-    children = int(input())
+    gross = float(input("Gross salary? : "))
+    children = int(input("How many children do you have?: "))
 
     if gross < 1000:
         tax = 0.10
@@ -10,11 +10,14 @@ try:
         tax = 0.14
     else:
         tax = 0.18
-
     if gross < 2000:
-        tax = tax - children * 0.01
+        tax = tax - (children * 0.01)
     else:
-        tax = tax - children * 0.005
+        tax = tax - (children * 0.005)
 
     net = gross - (gross * tax)
-    print(net)
+
+    print("Net salary is:", net)
+
+except:
+    print("Please give valid numbers")
